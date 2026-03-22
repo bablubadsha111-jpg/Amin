39d39df385d11db8ef7ed579390238216dfcb60d
bablubadsha111-jpg <bablubadsha111@gmail.com>
2026-03-22 16:50:25 +0530

    Initialize TTS model on first request

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ import base64
 
 app = FastAPI()
 
-# Load model (lightweight)
-tts = pipeline("text-to-speech", model="parler-tts/parler_tts_mini")
+tts = None  # initially empty
 
 class TextInput(BaseModel):
     text: str
 
 @app.get("/")
 def home():
-    return {"message": "TTS Server Running 🚀"}
+    return {"message": "Server Running ✅"}
 
 @app.post("/tts")
 def generate_tts(input: TextInput):
-    result = tts(input.text)
+    global tts
+    
+    if tts is None:
+        tts = pipeline("text-to-speech", model="facebook/mms-tts-eng")
 
+    result = tts(input.text)
     audio_bytes = result["audio"]
 
-    # Convert to base64 (important for API)
     audio_base64 = base64.b64encode(audio_bytes).decode("utf-8")
 
-    return {
-        "audio": audio_base64
-    }
+    return {"audio": audio_base64}
